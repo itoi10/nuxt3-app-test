8a7748c6ee92ace3c4c8b448776ddaa1d9b33b9b
itict10 <itict10@gmail.com>
2022-05-25 20:08:43 +0900

    setup Nuxt 3 app, ESLint

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const hello = ref<string>('Hello! Nuxt 3')
+
+const changeString = (newValue: string) => {
+  hello.value = newValue
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ hello }}</h1>
+    <button @click="changeString('こんにちは! Nuxt 3')">
+      Click!
+    </button>
+    <NuxtWelcome />
+  </div>
+</template>
